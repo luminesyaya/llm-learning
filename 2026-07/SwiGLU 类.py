121f515b4eb8fc7bb8141d6ea8734cce2d763a29
luminesyaya <2931287530@qq.com>
2026-07-20 10:17:57 +0800

    Add RoPE notes and SwiGLU FFN implementation

---
+++ 2026-07/SwiGLU 类.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SwiGLUFFN(nn.Module):
+    def __init__(self, hidden_size, intermediate_size):
+        super().__init__()
+        self.gate_proj = nn.Linear(hidden_size, intermediate_size, bias=False)
+        self.up_proj = nn.Linear(hidden_size, intermediate_size, bias=False)
+        self.down_proj = nn.Linear(intermediate_size, hidden_size, bias=False)
+    
+    def forward(self, x):
+        return self.down_proj((F.silu(self.gate_proj(x)) * self.up_proj(x)))
